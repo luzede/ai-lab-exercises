d2e7c8284c51dc379c09577bf8c7ced607710217
FilipposGiannakopoulos <78109143+FilipposGiannakopoulos@users.noreply.github.com>
2024-05-12 20:09:18 +0300

    New predicates for exactly the number asked.

--- predicates.pl
+++ predicates.pl
@@ -19,6 +19,17 @@ at_least_two_common_genres(Movie1, Movie2) :- common_genre(Movie1, Movie2, Genre
 at_least_three_common_genres(Movie1, Movie2) :- common_genre(Movie1, Movie2, Genre1), common_genre(Movie1, Movie2, Genre2), common_genre(Movie1, Movie2, Genre3), Genre1 \= Genre2, Genre1 \= Genre3, Genre2 \= Genre3.
 at_least_four_common_genres(Movie1, Movie2) :- common_genre(Movie1, Movie2, Genre1), common_genre(Movie1, Movie2, Genre2), common_genre(Movie1, Movie2, Genre3), common_genre(Movie1, Movie2, Genre4), Genre1 \= Genre2, Genre1 \= Genre3, Genre1 \= Genre4, Genre2 \= Genre3, Genre2 \= Genre4, Genre3 \= Genre4.
 
+exactly_one_common_genre(Movie1, Movie2) :- 
+    common_genre(Movie1, Movie2, Genre),
+    \+ (common_genre(Movie1, Movie2, Genre2), Genre2 @< Genre).
+
+exactly_three_common_genres(Movie1, Movie2) :- 
+    common_genre(Movie1, Movie2, Genre1), 
+    common_genre(Movie1, Movie2, Genre2), 
+    common_genre(Movie1, Movie2, Genre3),
+    Genre1 @< Genre2, Genre2 @< Genre3,
+    \+ (common_genre(Movie1, Movie2, Genre4), Genre4 \= Genre1, Genre4 \= Genre2, Genre4 \= Genre3).
+
 %for same director
 
 same_director(Movie1, Movie2) :- director(Movie1, Director), director(Movie2, Director), Movie1 \= Movie2.
@@ -30,11 +41,38 @@ at_least_two_common_keywords(Movie1, Movie2) :- common_plot_keyword(Movie1, Movi
 at_least_three_common_keywords(Movie1, Movie2) :- common_plot_keyword(Movie1, Movie2, Keyword1), common_plot_keyword(Movie1, Movie2, Keyword2), common_plot_keyword(Movie1, Movie2, Keyword3), Keyword1 \= Keyword2, Keyword1 \= Keyword3, Keyword2 \= Keyword3.
 at_least_four_common_keywords(Movie1, Movie2) :- common_plot_keyword(Movie1, Movie2, Keyword1), common_plot_keyword(Movie1, Movie2, Keyword2), common_plot_keyword(Movie1, Movie2, Keyword3), common_plot_keyword(Movie1, Movie2, Keyword4), Keyword1 \= Keyword2, Keyword1 \= Keyword3, Keyword1 \= Keyword4, Keyword2 \= Keyword3, Keyword2 \= Keyword4, Keyword3 \= Keyword4.
 
+exactly_three_common_keywords(Movie1, Movie2) :- 
+    common_plot_keyword(Movie1, Movie2, Keyword1), 
+    common_plot_keyword(Movie1, Movie2, Keyword2), 
+    common_plot_keyword(Movie1, Movie2, Keyword3),
+    Keyword1 @< Keyword2, Keyword2 @< Keyword3,
+    \+ (common_plot_keyword(Movie1, Movie2, Keyword4), Keyword4 \= Keyword1, Keyword4 \= Keyword2, Keyword4 \= Keyword3).
+
+exactly_five_common_keywords(Movie1, Movie2) :- 
+    common_plot_keyword(Movie1, Movie2, Keyword1), 
+    common_plot_keyword(Movie1, Movie2, Keyword2), 
+    common_plot_keyword(Movie1, Movie2, Keyword3),
+    common_plot_keyword(Movie1, Movie2, Keyword4),
+    common_plot_keyword(Movie1, Movie2, Keyword5),
+    Keyword1 @< Keyword2, Keyword2 @< Keyword3, Keyword3 @< Keyword4, Keyword4 @< Keyword5,
+    \+ (common_plot_keyword(Movie1, Movie2, Keyword6), Keyword6 \= Keyword1, Keyword6 \= Keyword2, Keyword6 \= Keyword3, Keyword6 \= Keyword4, Keyword6 \= Keyword5).
+
+
 %for same three actors
 common_actor(Movie1, Movie2, Actor) :- actor(Movie1, Actor), actor(Movie2, Actor), Movie1 \= Movie2.
 at_least_one_common_actor(Movie1, Movie2) :- common_actor(Movie1, Movie2, Actor1).
 at_least_two_common_actors(Movie1, Movie2) :- common_actor(Movie1, Movie2, Actor1), common_actor(Movie1, Movie2, Actor2), Actor1 \= Actor2.
 at_least_three_common_actors(Movie1, Movie2) :- common_actor(Movie1, Movie2, Actor1), common_actor(Movie1, Movie2, Actor2), common_actor(Movie1, Movie2, Actor3), Actor1 \= Actor2, Actor1 \= Actor3, Actor2 \= Actor3.
+
+exactly_one_common_actor(Movie1, Movie2) :-
+    common_actor(Movie1, Movie2, Actor1),
+    \+ (common_actor(Movie1, Movie2, Actor2), Actor2 @< Actor1).
+
+exactly_two_common_actors(Movie1, Movie2) :-
+    common_actor(Movie1, Movie2, Actor1),
+    common_actor(Movie1, Movie2, Actor2),
+    Actor1 @< Actor2,
+    \+ (common_actor(Movie1, Movie2, Actor3), Actor3 \= Actor1, Actor3 \= Actor2).
 
 %for same language
 same_language(Movie1, Movie2) :- language(Movie1, Language), language(Movie2, Language), Movie1 \= Movie2.
