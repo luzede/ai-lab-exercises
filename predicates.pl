c114569c529788047a715466abc4f736311e7cb8
FilipposGiannakopoulos <78109143+FilipposGiannakopoulos@users.noreply.github.com>
2024-05-12 20:26:34 +0300

    Fixed exercise requirements

--- predicates.pl
+++ predicates.pl
@@ -14,21 +14,12 @@ Decade: decade(Movie, Decade) - Movie is a movie and Decade is the decade of Mov
 %for common genres (some keywords common)
 
 common_genre(Movie1, Movie2, Genre) :- genre(Movie1, Genre), genre(Movie2, Genre), Movie1 \= Movie2.
-at_least_one_common_genre(Movie1, Movie2) :- common_genre(Movie1, Movie2, Genre).
-at_least_two_common_genres(Movie1, Movie2) :- common_genre(Movie1, Movie2, Genre1), common_genre(Movie1, Movie2, Genre2), Genre1 \= Genre2.
-at_least_three_common_genres(Movie1, Movie2) :- common_genre(Movie1, Movie2, Genre1), common_genre(Movie1, Movie2, Genre2), common_genre(Movie1, Movie2, Genre3), Genre1 \= Genre2, Genre1 \= Genre3, Genre2 \= Genre3.
-at_least_four_common_genres(Movie1, Movie2) :- common_genre(Movie1, Movie2, Genre1), common_genre(Movie1, Movie2, Genre2), common_genre(Movie1, Movie2, Genre3), common_genre(Movie1, Movie2, Genre4), Genre1 \= Genre2, Genre1 \= Genre3, Genre1 \= Genre4, Genre2 \= Genre3, Genre2 \= Genre4, Genre3 \= Genre4.
+exactly_one_common_genre(Movie1, Movie2) :- common_genre(Movie1, Movie2, Genre), \+ (common_genre(Movie1, Movie2, Genre2), Genre2 @< Genre).
 
-exactly_one_common_genre(Movie1, Movie2) :- 
-    common_genre(Movie1, Movie2, Genre),
-    \+ (common_genre(Movie1, Movie2, Genre2), Genre2 @< Genre).
+exactly_three_common_genres(Movie1, Movie2) :- common_genre(Movie1, Movie2, Genre1), common_genre(Movie1, Movie2, Genre2), common_genre(Movie1, Movie2, Genre3), Genre1 @< Genre2, Genre2 @< Genre3, \+ (common_genre(Movie1, Movie2, Genre4), Genre4 \= Genre1, Genre4 \= Genre2, Genre4 \= Genre3).
 
-exactly_three_common_genres(Movie1, Movie2) :- 
-    common_genre(Movie1, Movie2, Genre1), 
-    common_genre(Movie1, Movie2, Genre2), 
-    common_genre(Movie1, Movie2, Genre3),
-    Genre1 @< Genre2, Genre2 @< Genre3,
-    \+ (common_genre(Movie1, Movie2, Genre4), Genre4 \= Genre1, Genre4 \= Genre2, Genre4 \= Genre3).
+five_or_more_common_genres(Movie1, Movie2) :- common_genre(Movie1, Movie2, Genre1), common_genre(Movie1, Movie2, Genre2), common_genre(Movie1, Movie2, Genre3), common_genre(Movie1, Movie2, Genre4), common_genre(Movie1, Movie2, Genre5), Genre1 @< Genre2, Genre2 @< Genre3, Genre3 @< Genre4, Genre4 @< Genre5.
+
 
 %for same director
 
@@ -36,43 +27,21 @@ same_director(Movie1, Movie2) :- director(Movie1, Director), director(Movie2, Di
 
 %for same plot (some plot keywords common)
 common_plot_keyword(Movie1, Movie2, Keyword) :- plot_keyword(Movie1, Keyword), plot_keyword(Movie2, Keyword), Movie1 \= Movie2.
-at_least_one_common_keyword(Movie1, Movie2) :- common_plot_keyword(Movie1, Movie2, Keyword).
-at_least_two_common_keywords(Movie1, Movie2) :- common_plot_keyword(Movie1, Movie2, Keyword1), common_plot_keyword(Movie1, Movie2, Keyword2), Keyword1 \= Keyword2.
-at_least_three_common_keywords(Movie1, Movie2) :- common_plot_keyword(Movie1, Movie2, Keyword1), common_plot_keyword(Movie1, Movie2, Keyword2), common_plot_keyword(Movie1, Movie2, Keyword3), Keyword1 \= Keyword2, Keyword1 \= Keyword3, Keyword2 \= Keyword3.
-at_least_four_common_keywords(Movie1, Movie2) :- common_plot_keyword(Movie1, Movie2, Keyword1), common_plot_keyword(Movie1, Movie2, Keyword2), common_plot_keyword(Movie1, Movie2, Keyword3), common_plot_keyword(Movie1, Movie2, Keyword4), Keyword1 \= Keyword2, Keyword1 \= Keyword3, Keyword1 \= Keyword4, Keyword2 \= Keyword3, Keyword2 \= Keyword4, Keyword3 \= Keyword4.
 
-exactly_three_common_keywords(Movie1, Movie2) :- 
-    common_plot_keyword(Movie1, Movie2, Keyword1), 
-    common_plot_keyword(Movie1, Movie2, Keyword2), 
-    common_plot_keyword(Movie1, Movie2, Keyword3),
-    Keyword1 @< Keyword2, Keyword2 @< Keyword3,
-    \+ (common_plot_keyword(Movie1, Movie2, Keyword4), Keyword4 \= Keyword1, Keyword4 \= Keyword2, Keyword4 \= Keyword3).
+exactly_three_common_keywords(Movie1, Movie2) :- common_plot_keyword(Movie1, Movie2, Keyword1), common_plot_keyword(Movie1, Movie2, Keyword2), common_plot_keyword(Movie1, Movie2, Keyword3), Keyword1 @< Keyword2, Keyword2 @< Keyword3, \+ (common_plot_keyword(Movie1, Movie2, Keyword4), Keyword4 \= Keyword1, Keyword4 \= Keyword2, Keyword4 \= Keyword3).
 
-exactly_five_common_keywords(Movie1, Movie2) :- 
-    common_plot_keyword(Movie1, Movie2, Keyword1), 
-    common_plot_keyword(Movie1, Movie2, Keyword2), 
-    common_plot_keyword(Movie1, Movie2, Keyword3),
-    common_plot_keyword(Movie1, Movie2, Keyword4),
-    common_plot_keyword(Movie1, Movie2, Keyword5),
-    Keyword1 @< Keyword2, Keyword2 @< Keyword3, Keyword3 @< Keyword4, Keyword4 @< Keyword5,
-    \+ (common_plot_keyword(Movie1, Movie2, Keyword6), Keyword6 \= Keyword1, Keyword6 \= Keyword2, Keyword6 \= Keyword3, Keyword6 \= Keyword4, Keyword6 \= Keyword5).
+five_or_more_common_keywords(Movie1, Movie2) :- common_plot_keyword(Movie1, Movie2, Keyword1), common_plot_keyword(Movie1, Movie2, Keyword2), common_plot_keyword(Movie1, Movie2, Keyword3), common_plot_keyword(Movie1, Movie2, Keyword4), common_plot_keyword(Movie1, Movie2, Keyword5), Keyword1 @< Keyword2, Keyword2 @< Keyword3, Keyword3 @< Keyword4, Keyword4 @< Keyword5.
 
 
 %for same three actors
 common_actor(Movie1, Movie2, Actor) :- actor(Movie1, Actor), actor(Movie2, Actor), Movie1 \= Movie2.
-at_least_one_common_actor(Movie1, Movie2) :- common_actor(Movie1, Movie2, Actor1).
-at_least_two_common_actors(Movie1, Movie2) :- common_actor(Movie1, Movie2, Actor1), common_actor(Movie1, Movie2, Actor2), Actor1 \= Actor2.
-at_least_three_common_actors(Movie1, Movie2) :- common_actor(Movie1, Movie2, Actor1), common_actor(Movie1, Movie2, Actor2), common_actor(Movie1, Movie2, Actor3), Actor1 \= Actor2, Actor1 \= Actor3, Actor2 \= Actor3.
 
-exactly_one_common_actor(Movie1, Movie2) :-
-    common_actor(Movie1, Movie2, Actor1),
-    \+ (common_actor(Movie1, Movie2, Actor2), Actor2 @< Actor1).
+exactly_one_common_actor(Movie1, Movie2) :- common_actor(Movie1, Movie2, Actor1), \+ (common_actor(Movie1, Movie2, Actor2), Actor2 @< Actor1).
 
-exactly_two_common_actors(Movie1, Movie2) :-
-    common_actor(Movie1, Movie2, Actor1),
-    common_actor(Movie1, Movie2, Actor2),
-    Actor1 @< Actor2,
-    \+ (common_actor(Movie1, Movie2, Actor3), Actor3 \= Actor1, Actor3 \= Actor2).
+exactly_two_common_actors(Movie1, Movie2) :- common_actor(Movie1, Movie2, Actor1), common_actor(Movie1, Movie2, Actor2), Actor1 @< Actor2, \+ (common_actor(Movie1, Movie2, Actor3), Actor3 \= Actor1, Actor3 \= Actor2).
+
+exactly_three_common_actors(Movie1, Movie2) :- common_actor(Movie1, Movie2, Actor1), common_actor(Movie1, Movie2, Actor2), common_actor(Movie1, Movie2, Actor3), Actor1 @< Actor2, Actor2 @< Actor3, \+ (common_actor(Movie1, Movie2, Actor4), Actor4 \= Actor1, Actor4 \= Actor2, Actor4 \= Actor3).
+
 
 %for same language
 same_language(Movie1, Movie2) :- language(Movie1, Language), language(Movie2, Language), Movie1 \= Movie2.
